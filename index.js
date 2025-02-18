2cb2cfa329ee5fb487c49d3cb74d2d68a087cf05
[200~Richieconceptgit config --global user.name Richieconceptgit config --global user.name [200~Richieconcept <richieconcept01@gmail.com>
2025-02-18 15:59:20 +0100

    port

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json()); // Middleware to parse JSON requests
 
 // CORS middleware
 app.use(cors({
-  origin: 'https://bill-express-app.vercel.app/', // React app's local server
+  origin: 'https://bill-express-app.vercel.app', // React app's local server
   methods: 'GET,POST,PUT,DELETE',
 }));
 
